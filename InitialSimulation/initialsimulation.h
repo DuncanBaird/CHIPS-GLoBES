2fab566d38d89a1eb375a3b55e8718f9652ae246
duncanThinkpad <duncan.baird.16@ucl.ac.uk>
2019-10-17 19:49:52 +0100

    Initial simulation added into repo

---
+++ InitialSimulation/initialsimulation.h
@@ -0,0 +1,17 @@
+/*
+* Initial Simultion Header File
+*
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <gsl/gsl_math.h>    /* GNU Scientific library (required for root finding) */
+#include <gsl/gsl_roots.h>
+#include <gsl/gsl_errno.h>
+
+#include "../Globes/globes-3.2.17/globes/globes.h"   /* GLoBES library */
+#include "myio.h"            /* my input-output routines */
+
+// /home/duncan/Documents/Globes/globes-3.2.17/globes/globes.h
