81aad6baf328db25ddfdb548a6028ebcebf5ac2a
duncanThinkpad <duncan.baird.16@ucl.ac.uk>
2019-10-30 18:04:53 +0000

    Initialsmilaution removed ROOT wrapping

--- InitialSimulation/initialsimulation.c
+++ InitialSimulation/initialsimulation.c
@@ -219,7 +219,7 @@ double chiDCSpectral(int exp, int rule, int n_params, double *x, double *errors,
     chi2 += likelihood( true_rates_N[i], fit_rate,
                     true_rates_N[i] * (1.0 + true_rates_N[i]*square(sigma_binbin)) );
   }
-https://www.argos.co.uk/help/faq/72487
+
   /* Systematical part of chi^2 (= priors) */
   for (i=0; i < n_params; i++)
     chi2 += square(x[i] / errors[i]);
@@ -445,3 +445,36 @@ int main(int argc, char *argv[])
 
   return 0;
 }
+
+/* ROOT plotting test (executing file in ROOT)
+*
+*
+*/
+
+// TH1* chi2Hist = NULL;
+
+// void graphinghistograms(){
+
+//   TFile* f = TFile::Open("/home/duncan/Documents/CHIPS Repository/CHIPS-GLoBES/Graphing for 2019-10-24/20190824_000_confFile_20190824_104918_02POMs_NanobeaconB_6200mV_acceptanceTest_nano.root");
+//   TTree *t1 =  (TTree*)f->Get("CLBOpt_tree");
+//   TBranch *b1 = (TBranch*)t1->GetBranch("TimeStamp_ns");
+//   TCanvas *myCanvas = new TCanvas();
+//   myCanvas->SetGrid();
+//   chi2Hist = new TH1D("Time events modulo","Histrogram of events over time",100,0,500); //last numbers: number of bins, lower bound of bins, upper bound of bins
+//   chi2Hist->GetXaxis()->SetTitle("Timestamp ns");
+//   chi2Hist->GetYaxis()->SetTitle("Frequency of event timestamp");
+
+//   UInt_t TimeStamp_ns;
+//   t1->SetBranchAddress("TimeStamp_ns",&TimeStamp_ns);
+
+
+//   int entries = t1->GetEntries();
+//   for (int i=0;i<entries;i++){
+//     t1->GetEntry(i);
+//     chi2Hist->Fill(TimeStamp_ns%50000); //take modulo 50000 of each timestamp
+//   }
+
+//   chi2Hist->Draw();
+//   gPad->Update();
+//   TPaveStats *st = (TPaveStats*)chi2Hist->FindObject("stats");
+// }
