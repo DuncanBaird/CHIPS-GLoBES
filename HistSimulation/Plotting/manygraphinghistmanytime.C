c9da5bbc0296e70c1b1e04066e5ef716743d8ce5
duncanThinkpad <duncan.baird.16@ucl.ac.uk>
2019-11-21 11:59:58 +0000

    Added channel names to plot output histsimulation

--- HistSimulation/Plotting/manygraphinghistmanytime.C
+++ HistSimulation/Plotting/manygraphinghistmanytime.C
@@ -10,6 +10,27 @@ void printer3(int loop){
 
   const int number_histograms = 18;
 
+  char channel_arr[number_histograms][40]{
+    "Channel: #nc_bg",
+    "Channel: #nu_mu_bg",
+    "Channel: #nu_e_beam_qe",
+    "Channel: #nu_e_beam_nqe",
+    "Channel: #nu_e_signal_nqe",
+    "Channel: #nu_e_signal_qe",
+    "Channel: #anc_bg",
+    "Channel: #anu_e_beam_qe",
+    "Channel: #anu_e_beam_nqe",
+    "Channel: #anu_mu_bg",
+    "Channel: #anu_e_signal_qe",
+    "Channel: #anu_e_signal_nqe",
+    "Channel: #canc_bg",
+    "Channel: #canu_e_beam_qe",
+    "Channel: #canu_e_beam_nqe",
+    "Channel: #canu_mu_bg",
+    "Channel: #canu_e_signal_qe",
+    "Channel: #canu_e_signal_nqe",
+  };
+
   char buf[120];
   char str[120];
 
@@ -38,10 +59,16 @@ file.close();
 
   char buf2[25];
   char str2[25];
-  sprintf(buf2,"Time: %d Interaction channel %d",loop,k);
+  sprintf(buf2,"Time: %d for %s",loop,channel_arr[k]);
   strcpy(str2,buf2);
 
-  chi2Hist = new TH1D(str2,str2,86,0,86); //last numbers: number of bins, lower bound of bins, upper bound of bins
+  char buf7[25];
+  char str7[25];
+  sprintf(buf7,"Statistics; %d",k);
+  strcpy(str7,buf7);
+
+
+  chi2Hist = new TH1D(str7,str2,86,0,86); //last numbers: number of bins, lower bound of bins, upper bound of bins
   chi2Hist->GetXaxis()->SetTitle("Energy GeV");
   chi2Hist->GetYaxis()->SetTitle("Frequency");
   for(int i = 0; i != 86; ++i) {
